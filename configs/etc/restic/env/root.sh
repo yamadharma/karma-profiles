f00c1d0bfc80b35d55b5bda5c8a7fe91f4675f85
Dmitry S. Kulyabov <yamadharma@gmail.com>
2019-10-27 19:12:13 +0300

    Вс окт 27 19:12:13 MSK 2019

---
+++ configs/etc/restic/env/root.sh
@@ -0,0 +1,7 @@
+# B2 credentials.
+# Extracted settings so both systemd timers and user can just source this when want to work on my B2 backup.
+# See https://restic.readthedocs.io/en/latest/030_preparing_a_new_repo.html
+
+export RESTIC_REPOSITORY="sftp:root@st.sci.pfu.edu.ru:/n/backup/restic-repo/beetle.sci.pfu.edu.ru/root"
+# export RESTIC_PASSWORD_FILE="/etc/restic/pw.txt"
+export RESTIC_PASSWORD=""
